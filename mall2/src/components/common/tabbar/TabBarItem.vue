228f1c7fad5bb46ee3e33265061df15b40a654f6
wojiabaopi <1834964550@qq.com>
2020-08-19 22:25:10 +0800

    '修改'

---
+++ mall2/src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,80 @@
+<template>
+
+  <div class="tab-bar-item" @click="itemClick">
+    <!--    <img src="../../assets/img/tabbar/home1.png" alt="">-->
+    <!--    <div>首页</div>-->
+    <div v-if="!isActive">
+      <slot name="slot-img"></slot>
+    </div>
+    <div v-else>
+      <slot name="slot-img-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="slot-text"></slot>
+    </div>
+  </div>
+  <!--  <div class="tab-bar-item">-->
+  <!--    <img src="../../assets/img/tabbar/sort1.png" alt="">-->
+  <!--    <div>分类</div>-->
+  <!--  </div>-->
+  <!--  <div class="tab-bar-item">-->
+  <!--    <img src="../../assets/img/tabbar/sh-cart1.png" alt="">-->
+  <!--    <div>购物车</div>-->
+  <!--  </div>-->
+  <!--  <div class="tab-bar-item">-->
+  <!--    <img src="../../assets/img/tabbar/mine1.png" alt="">-->
+  <!--    <div>我的</div>-->
+  <!--  </div>-->
+
+
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+      // isActive: false
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.push(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+.tab-bar-item {
+  height: 49px;
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
--- mall2/src/router/index.js
+++ mall2/src/router/index.js
@@ -1,27 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+const Home = () => import('../views/home/Home')
+const ProFile = () => import('../views/profile/ProFile')
+const Sort = () => import('../views/category/Sort')
+const ShopCart = () => import('../views/shopcart/ShopCart')
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/home'
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/profile',
+      component: ProFile
+    },
+    {
+      path: '/shopcart',
+      component: ShopCart
+    },
+    {
+      path: '/sort',
+      component: Sort
+    }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
